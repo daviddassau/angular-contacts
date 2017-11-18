4d8aa47b3c2cb6d01fe91121f782f953725a01a1
David Dassau <todieahero@hotmail.com>
2017-11-17 21:10:58 -0600

    deployed to firebase

--- javascripts/controllers/ViewCtrl.js
+++ javascripts/controllers/ViewCtrl.js
@@ -11,5 +11,13 @@ app.controller("ViewCtrl", function ($rootScope, $scope, ContactService) {
     };
 
     getContacts();
+
+    $scope.deleteContact = (contactId) => {
+        ContactService.deleteContact(contactId).then((result) => {
+            getContacts();
+        }).catch((error) => {
+            console.log("error in deleteContact", error);
+        });
+    };
     
 });
