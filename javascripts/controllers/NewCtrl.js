e2c31662a0ff07990e8091c419562fe52d1ca044
David Dassau <todieahero@hotmail.com>
2017-11-17 20:23:50 -0600

    finished step 2

--- javascripts/controllers/NewCtrl.js
+++ javascripts/controllers/NewCtrl.js
@@ -1,5 +1,16 @@
 "use strict";
 
-app.controller("NewCtrl", function ($scope) {
-    $scope.controller = "NewCtrl";
+app.controller("NewCtrl", function ($location, $rootScope, $scope, ContactService) {
+
+    $scope.submitForm = () => {
+        let newContact = $scope.contact;
+        $scope.contact.uid = $rootScope.uid;
+        console.log(newContact);
+        ContactService.addNewContact(newContact).then(() => {
+            $location.path('contacts/view');
+        }).catch((error) => {
+            console.log("error in submit", error);
+        });
+    };
+
 });
--- javascripts/controllers/ViewCtrl.js
+++ javascripts/controllers/ViewCtrl.js
@@ -1,5 +1,15 @@
 "use strict";
 
-app.controller("ViewCtrl", function ($scope) {
-    $scope.controller = "ViewCtrl";
+app.controller("ViewCtrl", function ($rootScope, $scope, ContactService) {
+    
+    const getContacts = () => {
+        ContactService.getUserContact($rootScope.uid).then((results) => {
+            $scope.contact = results;
+        }).catch((error) => {
+            console.log("error in getContacts", error);
+        });
+    };
+
+    getContacts();
+    
 });
